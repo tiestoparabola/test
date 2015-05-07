c5e452c570ee9e1f8a6a77d54f78b39f820f75b1
Ioram Gordadze <Yoh0xFF@gmail.com>
2015-05-08 01:24:03 +0400

    eightth commit

--- test/test.cc
+++ test/test.cc
@@ -4,6 +4,9 @@ using namespace std;
 
 int main() 
 {
+    vector<int> v(10);
+    for (int i = 0; i < 10; ++i) cin >> v[i];
+
     cout << "Hello World" << endl;
     cout << "Hola" << endl; 
     cout << "გამარჯობა" << endl;
