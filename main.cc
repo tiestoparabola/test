76bab9ce7d346eb4aa98bbc2081e702a5d63f749
Ioram Gordadze <Yoh0xFF@gmail.com>
2015-05-07 23:06:03 +0400

    Initial project version

---
+++ main.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Object 
+{
+    friend ostream & operator<<(ostream &os, const Object &obj);
+public:
+    Object();
+    Object(int x, int y);
+
+private:
+    int _x, _y;
+};
+
+void func();
+
+int main () 
+{
+    srand(time(NULL));
+
+    try 
+    {
+        func();
+    } catch (exception &ex) 
+    {
+        cout << ex.what() << endl;
+    }
+}
+
+ostream & operator<<(ostream &os, const Object &obj)
+{
+    cout << obj._x << ' ' << obj._y;
+}
+
+Object::Object()
+{
+    _x = _y = 0;
+}
+
+Object::Object(int x, int y) 
+{
+    _x = x; _y = y;
+    if (rand() % 2 == 0) throw invalid_argument( "received negative value");
+}
+
+void func() 
+{
+    Object obj(1, 2);
+    cout << obj << endl;
+}
